e75707c89ffdfc19bc64a9f3ea0e9a46ade6e2f8
Lucía <luciapiedrabuena1@gmail.com>
2025-08-14 17:43:58 +0200

    Added sapphire and bandaid

--- src/main/java/net/nebulosacrafts/takecare/TakeCare.java
+++ src/main/java/net/nebulosacrafts/takecare/TakeCare.java
@@ -1,6 +1,7 @@
 package net.nebulosacrafts.takecare;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,20 +13,24 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.nebulosacrafts.takecare.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MOD_ID)
-public class ExampleMod
+@Mod(TakeCare.MOD_ID)
+public class TakeCare
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "takecare";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public TakeCare(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+
+        // Register the items
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -45,15 +50,20 @@ public class ExampleMod
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS){
+            event.accept(ModItems.BANDAID);
+        }
     }
 
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
---
+++ src/main/java/net/nebulosacrafts/takecare/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.nebulosacrafts.takecare.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nebulosacrafts.takecare.TakeCare;
+
+public class ModItems {
+
+    // Deferred register es una lista de algo, en este caso Item
+    // Cuando añadamos aquí los items se registarán en un momento específico para que Forge los inyecte
+    // Al mod
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TakeCare.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BANDAID = ITEMS.register("bandaid", () -> new Item(new Item.Properties()));
+
+    // Método estático para poder acceder a registrar los elementos en el eventBus que habrá en la clase main
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
