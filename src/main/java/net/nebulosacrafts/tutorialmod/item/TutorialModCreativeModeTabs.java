1cfa7b50e990da6d0df241187fc618564d06d4c5
Lucía <luciapiedrabuena1@gmail.com>
2025-08-16 12:22:54 +0200

    Custom loot tables
    - added sapphire ores with variants from different stones that drop raw sapphire

--- src/main/java/net/nebulosacrafts/tutorialmod/item/TutorialModCreativeModeTabs.java
+++ src/main/java/net/nebulosacrafts/tutorialmod/item/TutorialModCreativeModeTabs.java
@@ -27,6 +27,11 @@ public class TutorialModCreativeModeTabs {
                                 pOutput.accept(ModItems.BANDAID.get());
                                 pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                                 pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                                pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                                pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                                pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                                pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
                                 /*
                                 Para poner todos los objetos de ModItems en la misma Tab ->
                                 .displayItems((pParameters, pOutput) -> {
