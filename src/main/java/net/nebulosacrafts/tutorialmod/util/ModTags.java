78526605986f0e82b71235a2592111682b2dc368
Lucía <luciapiedrabuena1@gmail.com>
2025-08-17 15:40:58 +0200

    Custom tags

---
+++ src/main/java/net/nebulosacrafts/tutorialmod/util/ModTags.java
@@ -0,0 +1,28 @@
+package net.nebulosacrafts.tutorialmod.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.nebulosacrafts.tutorialmod.TutorialMod;
+
+public class ModTags {
+
+    public static class Blocks{
+
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
+        private static TagKey<Block> tag(String name){
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        private static TagKey<Item> tag(String name){
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+    }
+
+}
